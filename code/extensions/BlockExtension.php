c7d0c66137f812df0d9c8642e161d236536cde06
Sander Hagenaars <sanderhagenaars@Sanders-MacBook-Pro.local>
2017-02-08 19:50:24 +0100

    added simple config and extension

---
+++ code/extensions/BlockExtension.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sanderhagenaars
+ * Date: 08/02/2017
+ * Time: 19.40
+ */
+
+namespace NobrainerWeb\Blocks;
+
+
+class BlockExtension extends \DataExtension
+{
+
+}
